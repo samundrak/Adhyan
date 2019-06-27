269e9e0a9b17f433b06f0710ba807cda9f2db7e2
samundrak <samundrak@yahoo.com>
2019-06-27 21:37:23 +0545

    regen files

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,28 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const firestore = admin.firestore();
 firestore.settings({ timestampsInSnapshots: true });
+// admin.firestore.se
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+  response.send('Hello from Firebasess!');
 });
-// exports.generateContent = functions.storage
-//   .object()
-//   .onFinalize(async (object) => {
-//     console.log(object);
-//     return true;
-//   });
-exports.generateContent = functions.firestore
-  .document('users/{usersId}/books')
-  .onCreate(async (change) => {
-    console.log(change);
-    return true;
+exports.createUser = functions.firestore
+  .document('posts/{postId}')
+  .onWrite((snap, context) => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    const newValue = snap.data();
+    process.exit();
+    // access a particular field as you would any JS property
+    const name = newValue.name;
+    console.log(name);
+    // perform desired operations ...
   });
